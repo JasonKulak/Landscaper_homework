f0562d427e9ce5419a31088a65c1f6550326beb8
Jason Kulak <JasonKulak@yahoo.com>
2023-11-12 18:46:41 -0600

    user tools

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 //I have a landscaping business and I cut lawns with my teeth
 //I make $1/day cutting lawns and I can do this as much as I want
+//You can upgrade your tools when you have enough money
+//You win when you have a Team of Starving Students and $1000
 
 
-let Day = 0
-const Tool = ['Teeth']
+const Tool = [
+    {name: 'Teeth', revenue: 1, cost: 0}
+    {name: 'Rusty Scissors', revenue: 5, cost: 5} 
+    {name: 'Old-Timey Push Mower', revenue: 50, cost: 25}
+    {name: 'Battery-Powered Lawn Mower', revenue: 100, cost: 250}
+    {name: 'Team of Starving Students', revenue: 250, cost: 500'}
+]
 
 
 
