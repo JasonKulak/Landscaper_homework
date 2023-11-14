7c18ade65bdf31275f5f8b991a5d45cc9728b809
Jason Kulak <JasonKulak@yahoo.com>
2023-11-13 21:37:31 -0600

    landscaper: win scenario

--- app.js
+++ app.js
@@ -5,40 +5,42 @@
 //You win when you have a Team of Starving Students and $1000
 
 
-const Tools = [
-    {name: 'Teeth', revenue: 1, cost: 0}
-    {name: 'Rusty Scissors', revenue: 5, cost: 5} 
-    {name: 'Old-Timey Push Mower', revenue: 50, cost: 25}
-    {name: 'Battery-Powered Lawn Mower', revenue: 100, cost: 250}
-    {name: 'Team of Starving Students', revenue: 250, cost: 500'}
-]
+const tools = [
+    {name: 'your teeth', revenue: 1, cost: 0},
+    {name: 'rusty scissors', revenue: 5, cost: 5},
+    {name: 'old-timey push mower', revenue: 50, cost: 25},
+    {name: 'battery-powered lawn mower', revenue: 100, cost: 250},
+    {name: 'team of starving students', revenue: 250, cost: 500}
+];
 
 //what do the palyer(s) need to win?
 const player = {
-    money: 0
-    tool: 0
-}
+    money: 0,
+    tool: 0,
+    wonGame: false
+};
 
 //what happens when I mow a lawn?
 function mowLawn() {
     const tool = tools[player.tool];
-    alert(`You mowed a lawn with ${tool.name} and make ${tool.revenue} dollars`)
-    player.money += tool.generates
+    alert(`You mowed a lawn with ${tool.name} and made ${tool.revenue} dollars`);
+    player.money += tool.revenue;
 }
 
 //what happens when I upgrade my tools?
 //what happens when I run out of tools? (if statement/conditional)
+//Do I have enough money?
 function upgrade(){
     if (player.tool + 1 < tools.length){
     const nextTool = tools[player.tool + 1];
-    if (nextTool.price < player.money){
-        money -= nextTool.price
+    if (nextTool.cost <= player.money){
+        player.money -= nextTool.cost;
         player.tool += 1;
     } else {
-        alert("not enough money to upgrade")
+        alert("not enough money to upgrade");
     }
     } else {
-        alert("Out of upgrades");
+        alert("No more tools to upgrade");
     }
 }
 
@@ -48,4 +50,24 @@ function winConditions(){
         alert("You have won the game")
         player.wonGame = true
     }
+}
+
+alert("Welcome to Landscaper!!!");
+
+while(!player.wonGame){
+    const response = prompt(`You currently have ${player.money} dollars, do you want to [m]ow lawns or [u]pgrade`);
+
+    if (response === 'm'){
+        mowLawn()
+    }
+
+    if (response === 'u'){
+        upgrade()
+    }
+
+    if (response !== 'u' && response !== 'm'){
+        alert("that's not a valid option, type 'm' or 'u'")
+    }
+
+    winConditions()
 }
